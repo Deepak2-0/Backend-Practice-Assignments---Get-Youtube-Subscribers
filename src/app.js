0f427baf3829e15aa58078f59906df4623f687bc
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-12-11 22:58:39 +0530

    initial changes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,37 @@
 
 const express = require('express');
 const app = express()
+const Subscribers  = require("./models/subscribers");
 
-
+ 
 // Your code goes here
+app.get("/", (req,res)=>{
+    res.send("hello");
+});
+
+app.get("/subscribers", async(req,res)=>{
+
+    let subscriber = await Subscribers.find({});
+    //console.log(subscriber);
+    res.send(subscriber);
+})
+
+app.get("/subscribers/names", async(req,res)=>{
+
+    let names = await Subscribers.find({}, {_id:0, name:1, subscribedChannel:1});
+    //console.log(subscriber);
+    res.send(names);
+})
 
 
+app.get("/subscribers/:id", async(req,res)=>{
 
+    let id  = req.params.id;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let subscriber = await Subscribers.find({_id:id});
+    //console.log(subscriber);
+    res.send(subscriber);
+    //res.send(id);
+})
 
 module.exports = app;
